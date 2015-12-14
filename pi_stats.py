589654e816934cb3b52d0b4897332a48f6a6be66
benokkema <benokkema@gmail.com>
2015-12-14 02:03:38 -0600

    add cpu core voltage

--- pi_stats.py
+++ pi_stats.py
@@ -1,15 +1,25 @@
 #rPi CPU data
 
 from subprocess import PIPE, Popen
+import psutil
 
+#returns cpu temp
 def get_CPU_temp():
 	p = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
 	o, _error = p.communicate()
-	return o[:]
+	return float(o[5:-2])
 	
+#returns core volatage
+def get_core_voltage():
+	p  = Popen(['vcgencmd', 'measure_volts', 'core'], stdout=PIPE)
+	o, _error = p.communicate()
+	return o
+
 def main():
 	cpu_temp = get_CPU_temp()
+	core_voltage = get_core_voltage()
 	print(cpu_temp)
+	print(core_voltage)
 
 if __name__ == '__main__':
 	main()
